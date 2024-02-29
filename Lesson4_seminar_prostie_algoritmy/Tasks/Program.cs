3e4f5ff54cee6cab4f1e9f79386fb7c434476e7e
Sidorov <sidorovn.n@yandex.ru>
2024-03-01 01:06:06 +0300

    Добавили задачу1

--- Lesson4_seminar_prostie_algoritmy/Tasks/Program.cs
+++ Lesson4_seminar_prostie_algoritmy/Tasks/Program.cs
@@ -1,4 +1,13 @@
 ﻿// Напишите программу, котороая принимает на вход трехзначное число и 
 // удаляет 2-ую цифру этого числа  
 
-int num = new Random() 
+//Справка: для того что бы заработали подсказки при печатании команд в терминале требуется
+// вводим dotnet sln list
+// далее dotnet sln add (укзаывается имя файла с раширением "csproj")
+// в моем случае это "Tasks.csproj"
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+int s = num / 100*10 + num%10;
+Console.WriteLine(s);
+Console.WriteLine(num / 100 + "" + num%10);
