30ea52255e9a2a40022e5aeb088a40b4c75fcf54
Sidorov <sidorovn.n@yandex.ru>
2024-03-02 22:01:12 +0300

    Добавил решение по задаче 4

---
+++ Lesson4_seminar_prostie_algoritmy/Homework_Prostie_algoritmy/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4.
+// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую
+
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(value: Console.ReadLine());
+int i = 1;
+while (i < n) 
+{
+    Console.Write($"{i}, ");
+     i++;
+}
+Console.Write("\b\b "); 
