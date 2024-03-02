df5f5d017d72b7a9d9a2cbedca95cecaf262f36c
Sidorov <sidorovn.n@yandex.ru>
2024-03-02 21:45:22 +0300

    Добавили решение по задаче 3

---
+++ Lesson4_seminar_prostie_algoritmy/Homework_Prostie_algoritmy/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3.
+// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.WriteLine("Введите двузначное чило: ");
+int q = Convert.ToInt32(value: Console.ReadLine());
+while  (true) 
+{
+    if ((q <= 99) && (q >= 10)) break;
+    Console.WriteLine("Введеное число не соответствует условиям");
+    Console.Write("Введите двузначное число N: ");
+q = Convert.ToInt32(Console.ReadLine());
+}
+int q1 = q / 10;
+int q2 = q % 10;
+if (q1 == q2) {Console.WriteLine("Числа равны");}
+else
+{if (q1 > q2) {Console.WriteLine($"Наибольшее число - {q1}");}
+else {Console.WriteLine($"Наибольшее число - {q2}");}}
